25545867f6a52a31b62644d9d4ad76e08789fce5
Your Name <you@example.com>
2019-08-24 19:46:46 +0530

    Added slf4j logging

--- src/main/java/ApiCheck.java
+++ src/main/java/ApiCheck.java
@@ -4,11 +4,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
 import com.google.gson.Gson;
 
 public class ApiCheck {
+
+	private static final Logger log = LoggerFactory.getLogger(ApiCheck.class);
+
 	public static void main(String[] args) {
 
 		RestTemplate restTemplate = new RestTemplate();
@@ -17,7 +22,7 @@ public class ApiCheck {
 		try {
 			reader = new BufferedReader(new FileReader(args[0]));
 			String name = reader.readLine();
-			System.out.println("Success fully load  the file is " + args[0]);
+			log.info("Success fully load  the file is " + args[0]);
 
 			PrintWriter pWrite = new PrintWriter("pass.txt");
 			PrintWriter fWrite = new PrintWriter("fail.txt");
@@ -27,16 +32,16 @@ public class ApiCheck {
 				String body = restTemplate.getForObject(
 						"http://api.casey.vic.gov.au/property?term=" + name + "&status=C&bin_colllection_day=true",
 						String.class);
-				System.out.println("success fully hit the API for the name " + name);
+				log.info("success fully hit the API for the name " + name);
 
 				ApiModel apiModel = gson.fromJson(body, ApiModel.class);
 
 				if (apiModel.getResult().length > 0) {
-					System.out.println("\u001B[32m" + "pass : " + body + "\u001B[0m");
+					log.info("\u001B[32m" + "pass : " + body + "\u001B[0m");
 					pWrite.println(name);
 				} else {
 
-					System.out.println("\u001B[31m" + "fail : " + body + "\u001B[0m");
+					log.info("\u001B[31m" + "fail : " + body + "\u001B[0m");
 					fWrite.println(name);
 				}
 				name = reader.readLine();
@@ -48,7 +53,7 @@ public class ApiCheck {
 			e.printStackTrace();
 		}
 
-		System.out.println("\u001B[32m"
+		log.info("\u001B[32m"
 				+ "Susscully program completed , you can see two files pass.txt and fail.txt in your current working directory"
 				+ "\u001B[0m");
 		// git remote add origin
